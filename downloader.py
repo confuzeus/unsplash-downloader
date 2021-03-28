e8536bda19e4c96731a8fca5aa761216af497fde
Michael Karamuth <michael@arsero.com>
2021-03-28 17:10:14 +0400

    Unsplash doesn't allow more than 30 images.
    Workaround.

--- downloader.py
+++ downloader.py
@@ -14,16 +14,18 @@ class Downloader:
         log.info("Initializing downloader.")
         access_key = os.getenv("UNSPLASH_ACCESS_KEY", sys.argv[1])
         self.headers = {"Authorization": "Client-ID " + access_key}
-        self.time_to_sleep = os.getenv(
-            "SLEEP_FOR", sys.argv[2]
+        self.time_to_sleep = int(
+            os.getenv("SLEEP_FOR", sys.argv[2])
         )  # Sleep for x seconds between each request.
         image_count = os.getenv("IMAGE_COUNT", sys.argv[3])
         self.destination_dir = os.getenv("DESTINATION_DIR", sys.argv[4])
 
+        # Unsplash allows fetching up to 30 images in one bundle.
+        # We have to iterate image_count / 30 times to get the desired amount.
+        self.iteration_count = image_count / 30
         self.unsplash_api_url = (
-            "https://api.unsplash.com/" f"photos/random/?count={image_count})"
-        )
-
+            "https://api.unsplash.com/" "photos/random/?count=30)"
+        )  # noqa
         if not os.path.isdir(self.destination_dir):
             log.info(f"Creating non-existant {self.destination_dir}.")
             os.makedirs(self.destination_dir, exist_ok=True)
@@ -36,13 +38,35 @@ class Downloader:
             )
         )
 
+    def log_errors(self, error_list, status_code=None):
+        if status_code:
+            log.error(f"Status code: {status_code}")
+        for error in error_list:
+            log.error(f"Error: {error}")
+
     def get_image_list(self):
-        res = requests.get(self.unsplash_api_url, headers=self.headers)
 
-        data = res.json()
+        image_list = []
 
-        if res.status_code == 200:
-            return data
+        def request_images():
+            return requests.get(self.unsplash_api_url, headers=self.headers)
+
+        for idx, __ in enumerate(range(self.iteration_count)):
+
+            log.info(f"Attempting to fetch batch {idx}")
+
+            res = request_images()
+            data = res.json()
+            if res.status_code == 200:
+                log.info("Success.")
+                image_list += [image_data for image_data in data]
+            else:
+                self.log_errors(data["errors"], res.status_code)
+                log.info("Will sleep before retrying")
+                time.sleep(self.time_to_sleep)
+                res = request_images()
+            log.info("Will sleep before next request.")
+            time.sleep(self.time_to_sleep)
 
         raise ConnectionError(f"Status code: {res.status_code}.")
 
@@ -71,7 +95,7 @@ class Downloader:
 
                     image_file.write(block)
             log.info("Success. Will now sleep.")
-            time.sleep(int(self.time_to_sleep))
+            time.sleep(self.time_to_sleep)
 
 
 if __name__ == "__main__":
