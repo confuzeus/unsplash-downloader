46f2a6f8f8a8949376e264ef04a9b77249d84727
Michael Karamuth <michael@arsero.com>
2021-03-28 16:30:46 +0400

    Added logging

--- downloader.py
+++ downloader.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import sys
 import time
@@ -5,18 +6,36 @@ import requests
 from urllib import parse
 
 
+log = logging.getLogger(__name__)
+
+
 class Downloader:
     def __init__(self):
+        log.info("Initializing downloader.")
         access_key = os.getenv("UNSPLASH_ACCESS_KEY", sys.argv[1])
         self.time_to_sleep = os.getenv(
             "SLEEP_FOR", sys.argv[2]
         )  # Sleep for x seconds between each request.
         image_count = os.getenv("IMAGE_COUNT", sys.argv[3])
         self.destination_dir = os.getenv("DESTINATION_DIR", sys.argv[4])
-        self.unsplash_api_url = f"https://api.unsplash.com/photos/random/?count={image_count}&client_id={access_key}"
+
+        self.unsplash_api_url = (
+            "https://api.unsplash.com/"
+            f"photos/random/?count={image_count})"
+            f"&client_id={access_key}"
+        )
 
         if not os.path.isdir(self.destination_dir):
+            log.info(f"Creating non-existant {self.destination_dir}.")
             os.makedirs(self.destination_dir, exist_ok=True)
+
+        log.info(
+            (
+                f"Amount of images to download: {image_count}.\n"
+                f"Sleep for seconds: {self.time_to_sleep}.\n"
+                f"Destination directory: {self.destination_dir}."
+            )
+        )
 
     def get_image_list(self):
         res = requests.get(self.unsplash_api_url)
@@ -31,6 +50,7 @@ class Downloader:
     def download_images(self):
 
         data = self.get_image_list()
+        log.info(f"Got array of length {len(data)}.")
 
         urls = [image_data["urls"]["full"] for image_data in data]
 
@@ -41,6 +61,7 @@ class Downloader:
             file_ext = parse.parse_qs(parsed.query)["fm"][0]
             file_name_with_ext = f"{file_name}.{file_ext}"
 
+            log.info(f"Downloading {file_name_with_ext}.")
             with open(
                 os.path.join(self.destination_dir, file_name_with_ext), "wb"
             ) as image_file:
@@ -50,7 +71,7 @@ class Downloader:
                         break
 
                     image_file.write(block)
-            print(f"Will sleep for {self.time_to_sleep} seconds.\n")
+            log.info("Success. Will now sleep.")
             time.sleep(int(self.time_to_sleep))
 
 
